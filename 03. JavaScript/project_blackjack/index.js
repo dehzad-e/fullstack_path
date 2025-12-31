bc36dcb6560b26b68479de2d2fba5b6d2b678cf1
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2025-12-31 00:47:20 -0500

    add the html and css files

--- 03. JavaScript/project_blackjack/index.js
+++ 03. JavaScript/project_blackjack/index.js
@@ -1,11 +1,18 @@
 let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard + 6 // 23
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
+    isAlive = false
 }
+
+console.log(message)
