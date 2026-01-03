abe18a41891751e0583baa8977890d57ff6105df
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-01-03 01:38:32 -0500

    add javascript code for the password generator app

--- 03. JavaScript/project_password_generator/index.js
+++ 03. JavaScript/project_password_generator/index.js
@@ -1,2 +1,24 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let password1El = document.getElementById("password1-el")
+let password2El = document.getElementById("password2-el")
+
+function generatePassword() {
+    let password1 = ""
+    let password2 = ""
+    for (let i = 0; i < 15; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        password1 += characters[randomIndex]
+    }
+    for (let i = 0; i < 15; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        password2 += characters[randomIndex]
+    }
+    password1El.textContent = password1
+    password2El.textContent = password2
+
+}
