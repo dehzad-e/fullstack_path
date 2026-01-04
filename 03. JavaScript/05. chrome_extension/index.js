7dbafe4517ea852290be79f82cb93eb0f12c2b60
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-01-04 00:22:44 -0500

    make the app get user input, save it to an array and render it on the screen

--- 03. JavaScript/05. chrome_extension/index.js
+++ 03. JavaScript/05. chrome_extension/index.js
@@ -1,10 +1,24 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push("www.awesomelead.com")
-    
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
