a008b6852dd0def2f2c651304acece9a23400b26
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-01-06 16:02:02 -0500

    render images

--- 03. JavaScript/concepts.js
+++ 03. JavaScript/concepts.js
@@ -1,44 +1,30 @@
-// The generateSentence(desc, arr) takes two parameterer: a description and an array.
-// It should return a string based upon the description and array.
+// Create a function that renders the three team images
+// Use a for loop, template strings (``), plus equals (+=)
+// .innerHTML to solve the challenge.
+const containerEl = document.getElementById("container")
+const imgs = [
+    "images/hip1.jpg",
+    "images/hip2.jpg",
+    "images/hip3.jpg"
+]
 
-// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
-// it should return the string: "The 3 largest countries are China, India, USA"
 
-// Example 2: If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
-// "The 2 best fruits are Apples, Bananas"
-
-// Use both a for loop and a template string to solve the challenge
-
-
-// First Method
-function generateSentence(desc, arr) {
-    let items = ""
-    for (let i = 0; i < arr.length; i++) {
-        items += arr[i]
-        if (i < (arr.length - 1)) {
-            items += ", "
-        }
+function renderIamges() {
+    for (let i = 0; i < imgs.length; i++) {
+        containerEl.innerHTML += `<img class="team-img" src="${imgs[i]}">`
     }
-    let sentence = (`The ${arr.length} ${desc} are ${items}`)
-    return sentence
 }
 
-console.log(generateSentence("largest countries", ["China", "India", "USA"]))
+renderIamges()
 
 
-// Second Method
-function generateSentence(desc, arr) {
-    let baseString = `The ${arr.length} ${desc} are `
-    const lastIndex = arr.length - 1
-    for (let i = 0; i < arr.length; i++) {
-        if (i === lastIndex) {
-            baseString += arr[i]
-        } else {
-            baseString += arr[i] + ", "   
-        }
+// Refactored - minimize manipulating the DOM as much as possible
+function renderImages() {
+    let imgsDOM = ""
+    for (let i = 0; i < imgs.length; i++) {
+        imgsDOM += `<img alt="Employee in the company" class="team-img" src="${imgs[i]}">`
     }
-    return baseString
+    container.innerHTML = imgsDOM
 }
 
-const sentence = generateSentence("highest mountains", ["Mount Everest", "K2"])
-console.log(sentence)
+renderImages()
