2f4ef3ae98ecff1152b890d779139cd16b3b4390
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-01-06 14:57:49 -0500

    finalize the lead-tracker chrome extension project

--- 03. JavaScript/index.js
+++ 03. JavaScript/index.js
@@ -1,21 +1,15 @@
-// SETTING THE STAGE
-const player = "Per"
-const opponent = "Nick"
-const game = "AmazingFighter"
-
-let points = 0
-let hasWon = false
-
-// PLAYING THE GAME
-points += 100
-hasWon = true
-
-// ANNOUNCING THE WINNER
-if (hasWon) {
-    console.log(`${player} got ${points} points and won the ${game} game!`)
-} else {
-    console.log(`The winner is ${opponent}! ${player} lost the game`)
+//.                parameters
+function greetUser(greeting, name) { 
+    welcomeEl.textContent = `${greeting}, ${name} 👋`
 }
 
-// Go through all variables and decide if they should be let or const
-// Change the console logs to use template strings instead of double quotes
+//.        arguments
+let hi = "Howdy"
+greetUser(hi, "James")
+
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+add(3, 4) 
