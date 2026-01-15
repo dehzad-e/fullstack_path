d74107b0ced0a4ce0e101a114ec3b2ec4d88efb0
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-01-14 23:59:04 -0500

    add JS code

--- 06. Eessential CSS/Projects/03. instragram_clone/index.js
+++ 06. Eessential CSS/Projects/03. instragram_clone/index.js
@@ -28,5 +28,46 @@ const posts = [
     }
 ]
 
+const postsContainer = document.getElementById("posts-container")
 
+let postsHTML = ""
+
+for (let i = 0; i < posts.length; i++) {
+    postsHTML += `
+        <article class="post container">
+            <section class="post-author">
+                <img class="avatar" src="${posts[i].avatar}" alt="${posts[i].name} profile picture">
+                <div class="author-info">
+                    <p class="author-name">${posts[i].name}</p>
+                    <p class="author-location">${posts[i].location}</p>
+                </div>
+            </section>
+            
+            <section class="post-image">
+                <img src="${posts[i].post}" alt="${posts[i].name}'s artwork post">
+            </section>
+            
+            <section class="post-details">
+                <div class="post-actions">
+                    <button class="action-btn" aria-label="Like post">
+                        <img src="images/icon-heart.png" alt="">
+                    </button>
+                    <button class="action-btn" aria-label="Comment on post">
+                        <img src="images/icon-comment.png" alt="">
+                    </button>
+                    <button class="action-btn" aria-label="Send direct message">
+                        <img src="images/icon-dm.png" alt="">
+                    </button>
+                </div>
+                <p class="likes-count">${posts[i].likes} likes</p>
+                <p class="post-caption">
+                    <span class="username">${posts[i].username}</span> 
+                    <span class="caption-text">${posts[i].comment}</span>
+                </p>
+            </section>
+        </article>
+`
+}
+
+postsContainer.innerHTML = postsHTML
 
