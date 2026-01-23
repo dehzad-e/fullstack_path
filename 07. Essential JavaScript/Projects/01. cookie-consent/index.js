00230b9d84f9df49f7d6a2497d886dd4aebcbbd1
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-01-22 21:50:15 -0500

    add the final message to the modal

--- 07. Essential JavaScript/Projects/01. cookie-consent/index.js
+++ 07. Essential JavaScript/Projects/01. cookie-consent/index.js
@@ -17,9 +17,20 @@ consentForm.addEventListener('submit', function(e){
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
         <p id="upload-text">Uploading your data to the dark web...</p>
-    </div>`
+    </div>` 
+    
     setTimeout(function(){
-        document.getElementById('upload-text').innerText = `Making the sale...`
-    }, 1500)   
-
-})
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    ` 
+    }, 3000)
+}) 
