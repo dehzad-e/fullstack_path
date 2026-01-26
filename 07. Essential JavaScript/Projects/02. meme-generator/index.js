3b68eca53e67704f2b20d5b694ed880e9dcef807
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-01-25 22:49:02 -0500

    render the emtoions

--- 07. Essential JavaScript/Projects/02. meme-generator/index.js
+++ 07. Essential JavaScript/Projects/02. meme-generator/index.js
@@ -169,14 +169,41 @@ const catsData = [
     },
 ]
 
+const emotionRadios = document.getElementById('emotion-radios')
+
 function getEmotionsArray(cats){
     const emotionsArray = []
-    
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
             emotionsArray.push(emotion)
         }
-        
     }
     return emotionsArray
 }
+
+/*
+Challenge:
+1. Take control of the 'emotion-radios' div.
+2. In renderEmotionsRadios, set up a let 
+   to hold our string of HTML. You can initialise
+   it with an empty string.
+3. Iterate over "emotions" and put each emotion 
+   in a <p> tag and then add them to the let you 
+   created in step 2. 
+4. Render the string to the 'emotion-radios' div.
+*/ 
+
+function renderEmotionsRadios(cats){
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
+        radioItems += `<p>${emotion}</p>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
+
+
+
+
