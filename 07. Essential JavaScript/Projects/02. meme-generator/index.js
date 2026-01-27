6e59921e23c2c0f6dbb2b038e7b12707f08e5f41
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-01-27 14:48:05 -0500

    remove the emotionsArrey duplicates by using .includes() array method

--- 07. Essential JavaScript/Projects/02. meme-generator/index.js
+++ 07. Essential JavaScript/Projects/02. meme-generator/index.js
@@ -1,40 +1,46 @@
-import { catsData } from "./data"
+import { catsData } from './data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
 function getEmotionsArray(cats){
     const emotionsArray = []
+        
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+/*
+Challenge:
+1. Refactor this nested for of so that an 
+   emotion is only pushed to emotionsArray
+   if it is not already in emotionsArray.
+   Extra kudos if you use the "logical not"
+   operator - feel free to google it!
+*/
+            if (!emotionsArray.includes(emotion)) {
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
 }
 
-/*
-Challenge:
-1. Take control of the 'emotion-radios' div.
-2. In renderEmotionsRadios, set up a let 
-   to hold our string of HTML. You can initialise
-   it with an empty string.
-3. Iterate over "emotions" and put each emotion 
-   in a <p> tag and then add them to the let you 
-   created in step 2. 
-4. Render the string to the 'emotion-radios' div.
-*/ 
 
 function renderEmotionsRadios(cats){
+        
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
     }
     emotionRadios.innerHTML = radioItems
 }
 
 renderEmotionsRadios(catsData)
-
-
-
-
