fea877b3b6c1ac6dc9ff0f4dc639475d7f1bb20d
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2026-02-03 23:56:37 -0500

    class list add/remove

--- 07. Essential JavaScript/Projects/02. meme-generator/concepts.js
+++ 07. Essential JavaScript/Projects/02. meme-generator/concepts.js
@@ -1,41 +1,9 @@
-const container = document.getElementById('container')
-
-const products = [
-    {
-        name: 'Ostrich Pillow',
-        price: '10',
-        image: 'ostrichpillow.jpg',
-        id: 'ostrich-pillow'
-    },
-    {
-        name: 'Bacon Bandages',
-        price: '8',
-        image: 'bacon-bandage.jpg',
-        id: 'bacon-bandages'
-    },
-    {
-        name: 'Baby Mop',
-        price: '20',
-        image: 'babymop.jpg',
-        id: 'baby-mop'
-    }
-]
-
-let productsHtml = ``
-
-for (let product of products){
-    productsHtml += `
-    <div class="product">
-        <h3>${product.name}</h3>
-         <h4> £${product.price}</h4>
-        <img src="${product.image}">
-        <button id="${product.id}">Buy Now</button>
-    </div>
-    `
-}
-
-container.innerHTML = productsHtml
-
-container.addEventListener('click', function(e){
-    document.getElementById(e.target.id).parentElement.style.backgroundColor = 'lightblue'
-}
+document.addEventListener('click', function(e){
+/*
+Challenge:
+1. Remove the "unread" class from the 
+   parentElement of the clicked element.
+*/
+    document.getElementById(e.target.id).parentElement.classList.add('read')
+    document.getElementById(e.target.id).parentElement.classList.remove('unread')
+})
